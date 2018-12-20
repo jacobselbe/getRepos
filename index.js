cc74bd596f538a3a00f48486d46a1311231666e2
Jacob Selbe <jalxselbe@gmail.com>
2018-12-20 01:27:32 -0600

    skeleton html and css files

---
+++ index.js
@@ -0,0 +1,40 @@
+
+//function to watch form
+    //capture and assign const user input
+    //print user input
+    //run api request function
+function watchform() {
+    $('#js-form').submit(e => {
+        e.preventDefault();
+        const user = $('#js-user').val();
+        console.log(user);
+        getUser(user);
+    });
+}
+
+//function to send request to api
+    //const endpoint using user input
+    //print endpoint
+    //api key?
+    //run fetch
+function getUser(user) {
+    const endpoint = `https://api.github.com/users/:${user}/repos`;
+    fetch(endpoint)
+        .then(response => response.json())
+        // .then(responseJson.articles => displayRepos(responseJson.articles))//??????
+        .catch(error => console.log(error));
+}
+
+//function to handle response from api
+    //print response
+    //update html
+function displayRepos(response) {
+    console.log(response);
+    $('#js-results').html(``);
+}
+
+//set page(watch form and print page ready)
+$(setPage => {
+    watchform();
+    console.log('page ready')
+});
