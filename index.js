64d15b924f6e0e4ceb0672de8ca9ba89b036fae1
Jacob Selbe <jalxselbe@gmail.com>
2018-12-20 20:02:22 -0600

    linked to github api and basic styling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 //function to watch form
+    //clear current html and input box
     //capture and assign const user input
     //print user input
     //run api request function
@@ -8,6 +9,8 @@ function watchform() {
         e.preventDefault();
         const user = $('#js-user').val();
         console.log(user);
+        $('#js-results').html('');
+        $('#js-user').val('');
         getUser(user);
     });
 }
@@ -18,19 +21,29 @@ function watchform() {
     //api key?
     //run fetch
 function getUser(user) {
-    const endpoint = `https://api.github.com/users/:${user}/repos`;
+    const endpoint = `https://api.github.com/users/${user}/repos`;
     fetch(endpoint)
         .then(response => response.json())
-        // .then(responseJson.articles => displayRepos(responseJson.articles))//fat arrow not working?
+        .then(responseJson => displayRepos(responseJson))
         .catch(error => console.log(error));
 }
 
 //function to handle response from api
     //print response
-    //update html
+    //for loop to update html with each item in the array
 function displayRepos(response) {
     console.log(response);
-    $('#js-results').html(``);//complete this 
+    $('#js-results').html(`
+        <h3>Repos for user: ${response[0].owner.login}</h3>
+        <p>Click a link below to see more!</p>
+    `);
+    for (let i = 0; i < response.length; i++) {
+        $('#js-results').append(`
+            <li>
+                <a href=${response[i].html_url} target="_blank">${response[i].name}</a>
+            </li>
+        `);
+    }
 }
 
 //set page(watch form and print page ready)
